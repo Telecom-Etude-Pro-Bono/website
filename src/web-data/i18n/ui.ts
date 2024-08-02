cc264e23043e1d4f38a12a2f59666889dee9dc5d
garnierk <Kevin_Garnier@outlook.fr>
2024-08-03 01:08:00 +0200

    end of index page

--- src/web-data/i18n/ui.ts
+++ src/web-data/i18n/ui.ts
@@ -8,6 +8,7 @@ export const showDefaultLang = false;
 
 export const ui = {
     fr: {
+        /* ################ NAV ###############*/
         'nav.home': '',
         'nav.about': 'Qui sommes-nous ?',
         'nav.patron': 'Devenez mécène',
@@ -18,14 +19,46 @@ export const ui = {
         'nav.publications': 'Publications',
         'nav.sitemap': 'Plan du site',
 
-
+        /* ################ FOOTER ###############*/
         'footer.legal': 'Mentions légales',
         'footer.rights': 'Tous droits réservés',
         'footer.contact': 'Contacter Telecom Etude Pro Bono',
 
+        /* ################ SEARCH ###############*/
         'search.placeholder': 'Rechercher',
 
+
+        /* ################ ALT ###############*/
+        'alt.logo': 'Logo de Telecom Etude Pro Bono',
+        'alt.animated-logo': 'Logo animé de Telecom Etude Pro Bono',
+        'alt.search': 'Icone de recherche',
+        'alt.dquote': 'Guillemets',
+        'alt.x': 'Icone de croix',
+        'alt.menu': 'Icone de menu',
+        'alt.logo-text': 'Logo de Telecom Etude Pro Bono avec le nom',
+
+        /* ################ PAGES ###############*/
+        'page.index': 'Telecom Etude Pro Bono',
+
+        /* ################ INDEX ###############*/
+        'index.title':'big blah blah',
         'index.cause-button': 'Notre cause en détail',
+        'index.supporters': 'Ils nous soutiennent',
+        'index.patron': 'Devenez mécène',
+        'index.support': 'Nous soutenir',
+        'index.individual.title': 'Particuliers',
+        'index.individual.text': 'En donnant au Fonds de dotation du CHUGA, vous contribuez à des projets innovants et porteurs de sens. Pour témoigner de notre gratitude, nous avons élaboré un programme de reconnaissance spécifique pour remercier chaque don.',
+        'index.company.title': 'Entreprises',
+        'index.company.text': "Source de rencontres, de partage et d’innovation, la participation au Cercle permet de tisser des liens privilégiés avec le CHUGA et de vous associer à nos valeurs, nos ambitions et nos projets. L’adhésion est valable 12 mois et donne droit à des avantages réservés aux membres. En savoir plus : fonds-chuga@chu-grenoble.fr",
+
+        'index.president.word.1': "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed doeiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        'index.president.word.2': "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        'index.president.name': 'Prénom Nom',
+        'index.president.function':'Président de Telecom Etude Pro Bono',
+
+        'index.last-news':"Les dernières actualités de Telecom Etude Pro Bono",
+        'index.last-projects':"Nous avons besoin de vous pour financer des projets. Un grand merci par avance pour votre soutien !",
+
 
     },
 } as const;
